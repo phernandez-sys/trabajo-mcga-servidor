d9b1bbd6643f26377d1fdf31e0dfdcfead5deca3
Ciro Angeleri <ciroangeleri@gmail.com>
2019-11-28 14:00:28 -0300

    MCGA-19: server cleanup and set up mongoDB collection

--- src/index.js
+++ src/index.js
@@ -1,48 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
 
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const app = express()
+const url =
+  "mongodb+srv://cangeler:CA-555-97@cluster0-6zzbb.mongodb.net/test?retryWrites=true&w=majority";
 
-// const url = 'mongodb+srv://esteban:123calculadora@cluster0-ovnju.mongodb.net/test?retryWrites=true&w=majority';
+mongoose
+  .connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch(e => console.log("ERROR", e));
 
-// mongoose.connect(url, { useNewUrlParser: true }, function (error) {
-//   console.log(error)
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cors())
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Test server</h1>");
+});
 
-app.get('/', (req, res) => {
-  res.status(200).send("<h1>Test server</h1>")
-})
-
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const myUser = {
-    email: 'frare.esteban@gmail.com',
-    password: '123123'
-  }
+    email: "frare.esteban@gmail.com",
+    password: "123123"
+  };
   if (req.body.email !== myUser.email) {
     return res.status(400).send({
       error: true,
       message: "Email is not registered"
-    })
+    });
   }
   if (req.body.password !== myUser.password) {
     return res.status(400).send({
       error: true,
       message: "Password is not correct"
-    })
+    });
   }
   return res.status(200).send({
     success: true,
-    message: 'User logged successfully',
-    user: myUser,
-  })
-})
+    message: "User logged successfully",
+    user: myUser
+  });
+});
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log('Servidor corriendo en puerto 4000')
-})
+  console.log("Servidor corriendo en puerto 4000");
+});
