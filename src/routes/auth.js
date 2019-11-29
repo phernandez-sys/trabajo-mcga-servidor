77e02cf8150516f0dde38e07208ae74b4d7aca5b
Ciro Angeleri <ciroangeleri@gmail.com>
2019-11-29 14:24:19 -0300

    MCGA-19: add bad login case

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,6 +8,8 @@ auth.post("/", (req, res) => {
   //Get all products
   User.find(req.body, (err, docs) => {
     if (err) return res.status(500).send(err);
+    if (!docs.length)
+      return res.status(401).send({ docs: [], message: "Bad credentials" });
     const token = jwt.sign({ test: "test" }, secret);
     res.status(200).send({ docs, token });
   });
